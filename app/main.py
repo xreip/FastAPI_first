a104d9d46b5e1bd48cab31d6f8eadc9b25311370
xreip <pierre.mouffe@gmail.com>
2023-02-26 19:04:10 +0100

    QueryParam&StringValidation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Query
 from pydantic import BaseModel
 
 from config.config import initiate_database
@@ -18,6 +18,7 @@ def read_root():
 @app.get("/users")
 async def list_users():
     return {"message": "list user route"}
+
 
 # Before by id !
 
@@ -58,24 +59,31 @@ async def get_food(food_name: FoodEnum):
 
     return {"Your food": food_name, "message": "You like things..."}
 
+
 # Query Params
-fake_items_db = [{"item_name": "FOO"}, {"item_name": "BAR"}, {
-    "item_name": "FOBAR"}, {"item_name": "BARFOO"}, {"item_name": "BFAO"},]
+fake_items_db = [
+    {"item_name": "FOO"},
+    {"item_name": "BAR"},
+    {"item_name": "FOBAR"},
+    {"item_name": "BARFOO"},
+    {"item_name": "BFAO"},
+]
 
 
-@app.get("/items")  # /items?skip=2 | /items?limit=1 | /items?skip=1&limit=3
-async def list_items(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
+# @app.get("/items")  # /items?skip=2 | /items?limit=1 | /items?skip=1&limit=3
+# async def list_items(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip: skip + limit]
 
 
-@app.get("/items/{item_id}")
-async def get_item(item_id: str, q: str | None = None, short: bool = False):
-    item = {"item_id": item_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update({"description": "Lorem ipsum dolor sit."})
-    return item
+# @app.get("/items/{item_id}")
+# async def get_item(item_id: str, q: str | None = None, short: bool = False):
+#     item = {"item_id": item_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update({"description": "Lorem ipsum dolor sit."})
+#     return item
+
 
 """ 4. Request body """
 
@@ -87,21 +95,51 @@ class Item(BaseModel):
     tax: float | None = None
 
 
-@app.post("/items")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+# @app.post("/items")
+# async def create_item(item: Item):
+#     item_dict = item.dict()
+#     if item.tax:
+#         price_with_tax = item.price + item.tax
+#         item_dict.update({"price_with_tax": price_with_tax})
+#     return item_dict
 
 
-@app.put("/items/{item_id}")
-async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
+# @app.put("/items/{item_id}")
+# async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
+#     result = {"item_id": item_id, **item.dict()}
+#     if q:
+#         result.update({"q": q})
+#     return result
+
+
+""" 5. Query param & string validation """
+
+
+@app.get("/items")
+async def read_items(
+    q: str
+    | None = Query(
+        None,
+        min_lenght=3,
+        max_length=10,
+        title="Sample query string",
+        description="This is a description",
+        alias="item-query",
+    ),
+    required: str = Query(..., min_length=5),
+    q_array: list[str] = Query(["foo", "bar"]),
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
-        result.update({"q": q})
-    return result
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items/hidden")
+async def hidden_query(hidden_query: str | None = Query(None, include_in_schema=False)):
+    if hidden_query:
+        return {"hidden_query": hidden_query}
+    return {"hidden_query": "Not found"}
 
 
 # app.include_router(api_router) # ADD THE CENTRAL ROUTER
